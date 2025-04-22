ba44679e40b0209ce30e67b9d10dc3fd259738d6
Shubham Aswal <shubhamaswal@Shubhams-MacBook-Air.local>
2025-04-22 22:56:03 +0530

    build test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint errors during build
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
